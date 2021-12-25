4905524061ddef86ff5721d468b42ff3652907f3
tanakatanaka <aiuro2003haru@gmail.com>
2021-12-26 02:29:24 +0900

    音声関係の処理を修正

--- walking_project/Assets/script/UI/UISettingPopup.cs
+++ walking_project/Assets/script/UI/UISettingPopup.cs
@@ -18,6 +18,9 @@ public class UISettingPopup : MonoBehaviour
     protected float m_MasterSFXVolume;
 
     protected const float k_MinVolume = -80f;
+    protected const string k_MasterVolumeFloatName = "MasterVolume";
+    protected const string k_MusicVolumeFloatName = "MusicVolume";
+    protected const string k_MasterSFXVolumeFloatName = "MasterSFXVolume";
 
     public void Open()
     {
@@ -45,18 +48,21 @@ public class UISettingPopup : MonoBehaviour
     public void MasterVolumeChangeValue(float value)
     {
         m_MasterVolume = k_MinVolume * (1.0f - value);
+        mixer.SetFloat(k_MasterVolumeFloatName, m_MasterVolume);
         GameManager.Instance.I_gameInfo.GetWorKData().masterVolume = m_MasterVolume;
     }
 
     public void MusicVolumeChangeValue(float value)
     {
         m_MusicVolume = k_MinVolume * (1.0f - value);
+        mixer.SetFloat(k_MusicVolumeFloatName, m_MusicVolume);
         GameManager.Instance.I_gameInfo.GetWorKData().musicVolume = m_MusicVolume;
     }
 
     public void MasterSFXVolumeChangeValue(float value)
     {
         m_MasterSFXVolume = k_MinVolume * (1.0f - value);
+        mixer.SetFloat(k_MasterSFXVolumeFloatName, m_MasterSFXVolume);
         GameManager.Instance.I_gameInfo.GetWorKData().masterSFXVolume = m_MasterSFXVolume;
     }
 
